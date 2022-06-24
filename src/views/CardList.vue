92196176a3b5ec24c06bb204722bc2f2ccbdadac
Elias Neugebauer <eliasneugebauer2001@gmail.com>
2022-06-24 15:05:40 +0200

    added Delete Button

--- src/views/CardList.vue
+++ src/views/CardList.vue
@@ -1,5 +1,11 @@
 <template>
   <h1>A list of cards </h1>
+  <div>
+    <h1>Delete a Card</h1>
+  <input v-model="cardId" placeholder="ID">
+  <br>
+  <button type="submit" @click="deleteCard">Delete</button>
+  </div>
   <div v-for="card in deck" :key="card">
     <p> {{ card }}</p>
   </div>
@@ -12,10 +18,28 @@ export default {
   props: ['id'],
   data () {
     return {
-      deck: null
+      deck: null,
+      cardId: ''
     }
   },
   methods: {
+    deleteCard () {
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/cards/' + this.cardId
+      const raw = JSON.stringify({
+        cardId: this.cardId
+      })
+
+      const requestOptions = {
+        method: 'DELETE',
+        body: raw,
+        redirect: 'follow'
+      }
+
+      fetch(endpoint, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error))
+    },
     turnIntoJson () {
       this.deck = JSON.stringify(this.deck)
       this.deck = JSON.parse(this.deck)
